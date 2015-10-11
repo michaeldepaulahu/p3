b6cd1378c142c4531c2c072709c9ee3aa1f92ce7
“Michael <michaeldepaula@fas.harvard.edu>
2015-10-11 15:07:59 -0400

    implementing Xkcd Controller

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -25,11 +25,7 @@ Route::get('/home', function () {
 Route::get('/profile', 'ProfileController@showProfile');
 
 // Loading xkcd within Laravel Framework
-Route::any('/xkcd', function(){
-	$custom = new Xkcd('words');
-	$custom->session('nW', 'words'); 
-	return View::make('p2', array(
-         'status' => $custom->word_status,
-         'display' => $custom->display()
-    ));	
-}); 
+Route::any('/xkcd', 'XkcdController@showXkcd');
+
+
+
